de8080430cb9b445a740840a8415212392b3ae3d
sanjay51 <sanjay.verma.nitk@gmail.com>
2016-11-26 09:25:18 +0530

    Some more markup support

--- src/app/shared/markup-helper.service.ts
+++ src/app/shared/markup-helper.service.ts
@@ -6,11 +6,28 @@ export class MarkupHelperService {
   constructor() { }
 
   parseToHTML(markupText: string): string {
+    return this.replaceBoldItalic(this.replaceLi(this.replaceHeadings(markupText)));
+  }
+
+  replaceHeadings(markupText: string): string {
     return markupText
-      .replace(/====(.*)====/, "<h4>$1</h4>")
-      .replace(/===(.*)===/, "<h3>$1</h3>")
-      .replace(/==(.*)==/, "<h2>$1</h2>")
-      .replace(/=(.*)=/, "<h1>$1</h1><hr/>");
+      .replace(/\n(==== |====)(.*)====/g, "<br/><h4>$2</h4>")
+      .replace(/\n(=== |===)(.*)===/g, "<br/><h3>$2</h3>")
+      .replace(/\n(== |==)(.*)==/g, "<br/><h2>$2</h2>")
+      .replace(/\n(= |=)(.*)=/g, "<br/><h1>$2</h1><hr/>");
+
+  }
+
+  replaceLi(markupText: string): string {
+    return markupText
+      .replace(/\n\*(.*)/g, "<br/><li>$1</li>")
+  }
+
+  replaceBoldItalic(markupText: string): string {
+    return markupText
+      .replace(/\'\'\'\'\'(.*)\'\'\'\'\'/g, "<b><i>$1</i></b>")
+      .replace(/\'\'\'(.*)\'\'\'/g, "<b>$1</b>")
+      .replace(/\'\'(.*)\'\'/g, "<i>$1</i>")
   }
 
 }
